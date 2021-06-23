9af0c288886caeedfa732ba74c596347b9ad5570
goergk <grzegorzkaw124@gmail.com>
2021-06-23 10:48:24 +0200

    Backup

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from . import util
 import markdown2 
 
-
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
